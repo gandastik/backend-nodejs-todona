fd84201ca570241087b7dbec3e36cea38ef64115
gandastik <gandastik@gmail.com>
2021-04-14 13:47:41 +0700

    connect to mogodbatlas

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://dbUser:dbUserPassword@cluster0.6lqvl.mongodb.net/todona?retryWrites=true&w=majority",
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => {
-//     console.log("Database connected");
-//   });
-
 mongoose
-  .connect("mongodb://localhost:27017/todona", { useNewUrlParser: true })
+  .connect(
+    "mongodb+srv://dbUser:dbUserPassword@cluster0.6lqvl.mongodb.net/todona?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
   .then(() => {
-    console.log("Database Connected at port 27017");
+    console.log("Database connected");
   });
+
+// mongoose
+//   .connect("mongodb://localhost:27017/todona", { useNewUrlParser: true })
+//   .then(() => {
+//     console.log("Database Connected at port 27017");
+//   });
 
 app.use("/api", routes);
 
-// app.listen(process.env.PORT || 1000, () => {
-//   console.log("Server started");
-// });
-
-app.listen(1000, () => {
-  console.log("server is ready on port 1000");
+app.listen(process.env.PORT || 1000, () => {
+  console.log("Server started");
 });
+
+// app.listen(1000, () => {
+//   console.log("server is ready on port 1000");
+// });
